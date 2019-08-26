121e364a7499785c6be2f43e16ca1f562ce50e1a
Matthew Suh <mys34@cornell.edu>
2019-08-26 10:58:21 -0400

    js left nav

--- script/leftNav.js
+++ script/leftNav.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
     $(window).scroll(function() {
         var $height = $(window).scrollTop();
         var $offsetBackground = ($("#background").offset().top).toFixed();
+        var $offset = ($("#background").offset().top).toFixed();
+        var $offsetBackground = ($("#background").offset().top).toFixed();
+        var $offsetBackground = ($("#background").offset().top).toFixed();
+        var $offsetBackground = ($("#background").offset().top).toFixed();
+        var $offsetBackground = ($("#background").offset().top).toFixed();
+
         if($height > $offsetBackground) {
             $('.leftNavStyle').addClass('active');
         } else {
